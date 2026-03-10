c35af1b1cde91eb3ff02f2c7069d54e834b515b5
mohamedaziz-2005 <tissmohamedaziz@gmail.com>
2026-03-10 14:21:35 -0800

    eeee5ar5etddd

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const ConnectDB = require('./Config/ConnectDB')
 const cors = require('cors')
 const userRouter = require('./Routes/User')
 const productRouter = require('./Routes/Product')
-const commendRouter = require('./Routes/Commend')
+const commendRouter = require('./Routes/Commandes')
+
 
 
 
@@ -23,7 +24,7 @@ ConnectDB()
 app.use(express.json())
 app.use( '/api/user' ,userRouter)
 app.use('/api/product' ,productRouter)
-app.use('/api/commande' ,commendRouter)
+app.use('/api/commande', commendRouter )
    
 
 app.listen(process.env.port , console.log(`Server is runnig on the port ${process.env.port}`))
